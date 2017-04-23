5498e5232bd8cc670c25b24643a270444a33b690
skear <skear@192.168.1.15>
2017-04-23 21:34:19 +0100

    Beginning of family tree program for Data Structures Assignment

---
+++ src/Driver.java
@@ -0,0 +1,153 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Driver {
+	public String name;
+	public String result = "";
+	Scanner scan = new Scanner(System.in);
+	public ArrayList<Person> people = new ArrayList();
+	
+	public static void main(String[] args) 
+	{
+		new Driver().run();
+	}
+	
+	public void run()
+	{
+		try {
+			readFile();
+		} catch (FileNotFoundException e) {
+			System.out.println("Something went wrong reading the file");
+			e.printStackTrace();
+		}
+		System.out.println("File read successfully");
+		assignParents();
+		printMenu();
+	}
+	
+	public void readFile() throws FileNotFoundException
+	{
+		File userFile = new File("C:/Users/skear/workspace/FamilyTree/src/Data/peopleList");
+		Scanner sc = new Scanner(userFile);
+		String delims = " ";
+		while(sc.hasNextLine())
+		{
+			String person = sc.nextLine();
+			String[] personTokens = person.split(delims);
+			String name = personTokens[0];
+			char gender = personTokens[1].charAt(0);
+			int birthYear =  Integer.parseInt(personTokens[2]);
+			String parent1 = personTokens[3];
+			String parent2 = personTokens[4];
+			people.add(new Person(name, gender, birthYear, parent1, parent2, null, null));
+		}
+	}
+	
+	public void assignParents(){
+		for(int i = 0; i < people.size(); i++)
+		{
+			for(int k = 0; k < people.size(); k++)
+			{
+				if(people.get(i).parent1.equals(people.get(k).name))
+				{
+					people.get(i).setP_Parent1(people.get(k));
+				}
+				
+				if(people.get(i).parent2.equals(people.get(k).getName()))
+				{
+					people.get(i).p_Parent2 = people.get(k);
+				}
+				
+				//Dealing with unknown parents. These will live outside of the array
+				if(people.get(i).parent1.equals("?")){
+					people.get(i).setP_Parent1(new Person("Unknown", '?', 0000, "Unknown", "Unknown", null, null));
+				}
+				if(people.get(i).parent2.equals("?")){
+					people.get(i).setP_Parent2(new Person("Unknown", '?', 0000, "Unknown", "Unknown", null, null));
+				}
+			}
+		}
+	}
+	
+	public void printMenu(){
+		System.out.println("Main Menu");
+		System.out.println("Press 1 to look up parents");
+		System.out.println("Press 2 to look up all ancestors");
+		System.out.println("Press 0 to exit");
+		int choice = scan.nextInt();
+		mainMenu(choice);
+	}
+	
+	public void mainMenu(int choice){
+		while(choice != 0){
+			switch(choice){
+			case 1:
+				System.out.println("Enter the name you want to look up");
+				scan.nextLine();
+				name = scan.nextLine();
+				System.out.println(parentLookup(name));
+				printMenu();
+			break;
+			case 2:
+				System.out.println("Enter the name you want to look up");
+				scan.nextLine();
+				name = scan.nextLine();
+				Person p = findPerson(name);
+				if(!(p == null))
+				{
+					System.out.println(ancestorLookup(p));
+				}else{
+					System.out.println("Please enter a valid name");
+				}
+					
+				printMenu();
+			break;
+			case 3:
+				System.out.println("Press 3 to look up siblings");
+			}
+		System.out.println("Bye");
+		System.exit(0);
+		}
+	}
+	
+	
+	public String parentLookup(String name){
+		String result = "unknown";
+		for(int i = 0; i < people.size(); i++){
+			if(people.get(i).getName().equals(name)){
+				result = "The parents of " + people.get(i).getName() + " are " + people.get(i).getP_Parent1().getName() + " and " + people.get(i).getP_Parent2().getName();
+				return result;
+			}	
+		}
+		return result;	
+	}
+	
+	
+	public Person findPerson(String name){
+		for(int i = 0; i < people.size(); i++){
+			if(people.get(i).getName().equals(name)){
+				Person result = people.get(i);
+				System.out.println("Found person");
+				return(result);
+			}	
+		}
+		System.out.println("Didnt find person");
+		return null;
+	}
+
+	public String ancestorLookup(Person person){	
+		
+		if(!person.getParent1().equals("Unknown"))
+		{
+			ancestorLookup(person.getP_Parent1()); 
+			if(!person.getParent2().equals("Unknown"))
+			{
+				ancestorLookup(person.getP_Parent2()); 
+				result += ("The parents of " + person.getName() + " are " + person.getP_Parent1().getName() + " and " + person.getP_Parent2().getName() + ". \n");
+			}
+		}
+		return result;
+	}
+}
---
+++ src/Person.java
@@ -0,0 +1,64 @@
+
+public class Person {
+	String name;
+	char gender;
+	int birthYear;
+	String parent1;
+	String parent2;
+	Person p_Parent1;
+	Person p_Parent2;
+	
+	public Person(String name, char gender, int birthYear, String parent1, String parent2, Person p_Parent1,
+			Person p_Parent2) {
+		this.name = name;
+		this.gender = gender;
+		this.birthYear = birthYear;
+		this.parent1 = parent1;
+		this.parent2 = parent2;
+		this.p_Parent1 = p_Parent1;
+		this.p_Parent2 = p_Parent2;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+	public String getParent1() {
+		return parent1;
+	}
+	public void setParent1(String parent1) {
+		this.parent1 = parent1;
+	}
+	public String getParent2() {
+		return parent2;
+	}
+	public void setParent2(String parent2) {
+		this.parent2 = parent2;
+	}
+	public Person getP_Parent1() {
+		return p_Parent1;
+	}
+	public void setP_Parent1(Person p_Parent1) {
+		this.p_Parent1 = p_Parent1;
+	}
+	public Person getP_Parent2() {
+		return p_Parent2;
+	}
+	public void setP_Parent2(Person p_Parent2) {
+		this.p_Parent2 = p_Parent2;
+	}
+}
